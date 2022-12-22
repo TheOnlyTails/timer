ed45e735fedc2c939508304921e7215517f8599f
TheOnlyTails <theonlytails@theonlytails.com>
2022-12-22 14:09:58 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# timer
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/sveltejs-kit-template-default-syzfz7)
---
+++ src/routes/+page.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+  import { onMount } from "svelte"
+
+  let time = 60 * 4
+
+  $: minutes = Math.floor(time / 60)
+	onMount(() => {
+		setInterval(() => {
+			time--
+		}, 1000)
+	})
+</script>
+
+{minutes} : {time - minutes * 60}
+<button on:click={() => time = 60 * 4}>Reset</button>
